b2fc7410c35c13567b282a2b0eb1d5861286297c
league <league@imac-12.attlocal.net>
2021-07-30 16:59:42 -0700

    not done with crazy cat

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -2,11 +2,48 @@
 package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
+import java.util.Random;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		
+		Robot rob = new Robot();
+		rob.penDown();
+		rob.setSpeed(100);
+		
+		rob.setPenWidth(10);
+		while (0 < 100) {
+		String red = JOptionPane.showInputDialog ("enter red");
+		String blue = JOptionPane.showInputDialog ("enter blue");
+		String green = JOptionPane.showInputDialog ("enter green");
+		int redi = 0;
+		int bluei = 0;
+		int greeni = 0;
+		try {
+			redi = Integer.parseInt(red);
+			bluei = Integer.parseInt(blue);
+			greeni = Integer.parseInt(green);
+		} catch (Exception e) {
+			Random ran = new Random();
+			redi= ran.nextInt(255);
+			bluei= ran.nextInt(255);
+			greeni= ran.nextInt(255);
+		}
+		rob.setPenColor(redi, bluei, greeni);
+		rob.move(200);
+		rob.turn(90);
+		rob.move(200);
+		rob.turn(90);
+		rob.move(200);
+		rob.turn(90);
+		rob.move(200);
+		rob.turn(90);
+		rob.turn(5);
+	}
 		//1. Create a new Robot
 	    //2. Make the robot draw a shape (this will take more than one line of code)		
 		//3. Set the pen width to 10
--- src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
+++ src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
@@ -3,10 +3,27 @@ package _01_else_if._2_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
+import java.awt.Desktop;
+
+import java.net.URI;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
+		String cats = JOptionPane.showInputDialog ("Enter your number of cats");
+		 int catsi = Integer.parseInt(cats);
+		 if (catsi >= 3) {
+			 System.out.println("you are a crazy cat lady");
+		 }
+		 else if(catsi <= 3) {
+		playVideo("https://www.youtube.com/watch?v=2QMeGkbdIVw")	;
+		 }
+		 else if(catsi == 0) {
+			 playVideo("https://www.youtube.com/watch?v=oj_yLBltPE8");
+		 }
 		
-		// 1. Ask the user how many cats they have
+		 // 1. Ask the user how many cats they have
 		// 2. Convert their answer into an int
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
